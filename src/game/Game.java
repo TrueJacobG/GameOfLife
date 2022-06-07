e85f7368f1656ce18318668d7f6ef55e2703f09f
Jakub <jakubgradzewicz1309@gmail.com>
2022-06-07 19:51:42 +0200

    run button, color testing, simple start stop logic

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,37 +1,60 @@
 package game;
 
-import game.button.Field;
+import game.button.FieldButton;
+import game.button.RunGameButton;
 
 import javax.swing.*;
 import java.awt.*;
 
-import static game.constant.ScreenSize.*;
+import static game.constant.Size.*;
 
 public class Game {
-    public static void run(){
-        JFrame frame = new JFrame();
+    private static JButton[][] buttons;
+    public static JPanel screen;
+    public static JPanel grid;
+    public static JFrame frame;
 
+    public Game(){
+        frame = new JFrame();
+        frame.setResizable(false);
+        frame.setTitle("Game of Life");
         frame.setSize(WIDTH,HEIGHT);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel screen = new JPanel(new GridLayout(ROWS,COLS));
-        JButton[][] buttons = new JButton[ROWS][COLS];
+        screen = new JPanel();
+        screen.setLayout(new BoxLayout(screen, BoxLayout.Y_AXIS));
+        grid = new JPanel(new GridLayout(ROWS,COLS));
+        buttons = new JButton[ROWS][COLS];
 
         for(int i = 0; i < ROWS; i++){
             JButton[] row = new JButton[COLS];
             for(int j = 0; j < COLS; j++){
-                row[j] = new Field(String.valueOf(i*j));
+                row[j] = new FieldButton(i, j);
             }
             buttons[i] = row;
         }
 
         for(int i = 0; i < ROWS; i++){
             for(int j = 0; j < COLS; j++){
-                screen.add(buttons[i][j]);
+                grid.add(buttons[i][j]);
             }
         }
 
+        JPanel menu = new JPanel();
+        menu.add(new RunGameButton());
+
+        screen.add(grid);
+        screen.add(menu);
+
         frame.add(screen);
         frame.setVisible(true);
+    }
+
+    public static JButton[][] getButtons(){
+        return buttons;
+    }
+
+    public static JPanel getGrid(){
+        return grid;
     }
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,6 @@ import game.Game;
 
 public class Main {
     public static void main(String[] args) {
-        Game.run();
+        new Game();
     }
 }
